434d886425df375082d025e0d57a1bc7cf1edb65
Thanh Xuan Vu <vuthanhxuan99@gmail.com>
2021-04-29 09:17:23 +0700

    'listview'

--- app/src/main/java/com/example/baitapvenha/MainActivity.java
+++ app/src/main/java/com/example/baitapvenha/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.baitapvenha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button btnAddBookMain;
+    ListView listView;
+    BookAdapter bookAdapter;
+    ArrayList<Book> listBook = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.lvListBook);
+        btnAddBookMain = findViewById(R.id.btnAddBook);
+
+        bookAdapter = new BookAdapter(listBook, MainActivity.this);
+        listView.setAdapter(bookAdapter);
+
+        btnAddBookMain.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, AddBookScreen.class);
+        startActivity(intent);
+    }
+
 }
